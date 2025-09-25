e787cdd411d43199d1503be09b1839c73fdecce5
icebox <qqqdyz@gmail.com>
2025-09-26 00:05:01 +0800

    skib

---
+++ crypto/cathode-ray-tubes/chal.py
@@ -0,0 +1,15 @@
+from Crypto.Util.number import long_to_bytes, bytes_to_long, getPrime
+from os import urandom
+
+flag = 'DOHYO{' + urandom(16).hex() + '}'
+flag_bytes = bytes_to_long(flag.encode())
+coeffs = [flag_bytes] + [getPrime(20*8) for i in range(49)]
+
+def f(x):
+    s = 0
+    for power, coeff in enumerate(coeffs):
+        s += coeff * x ** power
+    return s
+
+for i in range(250):
+    print(f'f({i}) = {f(i)}')
